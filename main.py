151dece186173fbee85488e00ef60c91262e1025
Lucas Müller <lucasmullerm@gmail.com>
2017-10-26 06:19:29 +0000

    adding filtering for events

--- main.py
+++ main.py
@@ -2,14 +2,24 @@ import logging as log
 import os
 from prob import generate
 
+import operator
+
 log.basicConfig(filename='out.txt', level=log.DEBUG)
 
 SONGS_FOLDER = 'songs'
 
+def sorted_dict(d):
+    return sorted(d.items(), key=operator.itemgetter(1))
+
 def main():
     p = generate(SONGS_FOLDER)
-    for x in p.minorCount[0]:
-        log.info(str(x) + ': ' + str(p.minorCount[0][x]))
+    for x in sorted_dict(p.minorCount[2]):
+        log.debug(x)
+    log.debug('------------------ total minor: ' + str(p.minorTotal))
+    for x in sorted_dict(p.majorCount[2]):
+        log.debug(x)
+    log.debug('------------------ total major: ' + str(p.majorTotal))
+        # log.info(str(x) + ': ' + str(p.minorCount[2][x]))
 
 if __name__ == '__main__':
     main()
--- prob.py
+++ prob.py
@@ -38,6 +38,9 @@ class Event: # Note or Chord
     def __str__(self):
         return str(self.value)
 
+    def __repr__(self):
+        return self.__str__()
+
 class Probability:
     """
     Holds probability for events.
@@ -46,16 +49,29 @@ class Probability:
     def __init__(self):
         self.majorCount = [defaultdict(int) for i in range(3)]
         self.minorCount = [defaultdict(int) for i in range(3)]
-        self.total = 0
+        self.majorTotal = [0] * 3
+        self.minorTotal = [0] * 3
+
+    def filter(self):
+        for i in range(3):
+            # major
+            oldSize = len(self.majorCount[i])
+            self.majorCount[i] = defaultdict(int, {k: v for k, v in self.majorCount[i].items() if v > 1})
+            self.majorTotal[i] -= oldSize - len(self.majorCount[i])
+
+            #minor
+            oldSize = len(self.minorCount[i])
+            self.minorCount[i] = defaultdict(int, {k: v for k, v in self.minorCount[i].items() if v > 1})
+            self.minorTotal[i] -= oldSize - len(self.minorCount[i])
 
     # event holds prev and prev2 if the case
     # level = notes before used to predict
-    def P(self, event, mode, level):
+    def P(self, event, mode='major', level=0):
         assert not level or len(event) == level + 1
         if mode == MAJOR:
-            return self.majorCount[level][event] / self.total
+            return self.majorCount[level][event] / self.majorTotal[level]
         elif mode == MINOR:
-            return self.minorCount[level][event] / self.total
+            return self.minorCount[level][event] / self.minorTotal[level]
         else:
             raise "Invalid mode: " + str(mode)
 
@@ -69,13 +85,19 @@ class Probability:
         # Choose count dictionary
         if key.type == MAJOR:
             count = self.majorCount
+            for i in range(3):
+                self.majorTotal[i] += len(part.flat.notes)
         elif key.type == MINOR:
             count = self.minorCount
+            for i in range(3):
+                self.minorTotal[i] += len(part.flat.notes)
         else:
             raise 'did not recognize key'
 
         prev = Event(0)
         prev2 = Event(0) #2nd previous
+
+        # mapping from Note/Chord/Rest object to integers
         for event in part.flat.notes:
             if event.isNote:
                 num = Event(self.__getNoteNumber(tonic, event))
@@ -85,12 +107,13 @@ class Probability:
                 num = Event(0)
             else:
                 raise 'Invalid Type: ' + str(event)
-            
-            self.total += 1
+
+            # update counter for probabilities
             count[0][num] += 1
             count[1][(prev, num)] += 1
             count[2][(prev2, prev, num)] += 1
 
+            # update previous
             prev2 = prev
             prev = num
 
@@ -109,6 +132,7 @@ def generate(directory):
         filepath = os.path.join(directory, filename)
         log.info(filepath)
         p.include_file(filepath)
+    p.filter()
     return p
 
 def main():
