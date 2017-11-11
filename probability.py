d9bdf3ce54467ed0635c368b0a21ab4bceee3712
Lucas Müller <lucasmullerm@gmail.com>
2017-11-11 00:17:09 +0000

    add_track

--- probability.py
+++ probability.py
@@ -1,17 +1,17 @@
-from math import log2
+from math import log2, inf
 from collections import defaultdict
 from util import NOTES
 
 # Constants
 MAX_LEVEL_COND = 2
 MAX_LEVEL_DELTA = 2
-
+REST_MIDI = -inf
 class Probability(object):
     def __init__(self):
         self.total = 0
         self.duration = defaultdict(int) # log2
-        self.notes = [defaultdict(int) for i in range(MAX_LEVEL_COND + 1)]
-        self.delta = [defaultdict(int) for i in range(MAX_LEVEL_DELTA + 1)]
+        self.notes = [defaultdict(int) for i in range(MAX_LEVEL_COND)]
+        self.deltas = [defaultdict(int) for i in range(MAX_LEVEL_DELTA)]
 
     def noteP(self, note, level=0): # note(index) or tuple [conditional]
         assert self.total
@@ -24,4 +24,41 @@ class Probability(object):
     def deltaP(self, delta, level=1): # delta in pitch
         assert self.total
         assert self.level
-        return self.delta[level][note] / self.total
+        return self.deltas[level][delta] / self.total
+
+    def add_track(self, track):
+        store_total = self.total
+        prev_notes = [REST_MIDI] * MAX_LEVEL_COND
+        prev_midi = [REST_MIDI] * MAX_LEVEL_DELTA
+        for event in track:
+            self.total += 1
+            dur = event.duration.quarterLength
+            assert dur
+            if event.isNote:
+                note = NOTES[event.name]
+                midi = event.pitch.midi
+            if event.isRest:
+                note = REST_MIDI
+                midi = REST_MIDI
+            elif event.isChord: # get root note
+                note = NOTES[event.root().name]
+                midi = event.root().midi
+
+            # Add duration
+            self.duration[dur] += 1
+
+            # Add notes
+            for l in range(MAX_LEVEL_COND):
+                cur = tuple(notes[MAX_LEVEL_COND-1:]) + (note,)
+                self.notes[l][cur] += 1
+
+            # Add deltas
+            for l in range(1, MAX_LEVEL_DELTA):
+                delta = midi - prev_midi[-l]
+                self.deltas[l][delta] += 1
+
+            # Update previous
+            prev_notes = prev_notes[1:] + [note]
+            prev_midi = prev_midi[1:] + [midi]
+
+        log.info("adding track with %d notes.".format(self.total - store_total))
