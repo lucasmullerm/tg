7a7610c4e73d3e48c9253ee157453916182ee67e
Lucas Müller <lucasmullerm@gmail.com>
2017-11-12 18:36:53 -0200

    cleaning up

--- key.py
+++
@@ -1,6 +0,0 @@
-def getSongKey(filename):
-    score = converter.parse(filename)
-    key = score.analyze('key')
-    scale = key.getScale()
-    print('Scale:', scale.tonic.name)
-    # print(key.tonic.name, key.mode)
